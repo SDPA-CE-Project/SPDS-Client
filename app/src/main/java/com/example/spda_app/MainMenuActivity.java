a61beee0d1da2ce37216626bb98d111e66a958a2
eClassBox <kjy1120@tukorea.ac.kr>
2024-09-18 22:07:13 +0900

    databinding -> findviewbyid
    binding 문제를 해결 시도

--- app/src/main/java/com/example/spda_app/MainMenuActivity.java
+++ app/src/main/java/com/example/spda_app/MainMenuActivity.java
@@ -1,42 +1,54 @@
 package com.example.spda_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.view.PreviewView;
 
 import com.example.spda_app.DAO.DBManager;
-import com.example.spda_app.databinding.ActivityMainMenuBinding;
-import com.example.spda_app.databinding.MainPageBinding;
 import com.google.firebase.auth.FirebaseAuth;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 import android.widget.TextView;
 import android.widget.ImageView;
 
 public class MainMenuActivity extends AppCompatActivity {
-    private MainPageBinding binding;
+
+
+    private Button start;
+    private ImageButton profile, setting;
+    private ImageView logStatus;
+
+    private TextView log;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        binding = MainPageBinding.inflate(getLayoutInflater());
 
-        setContentView(binding.getRoot());
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_page);
+
+        start = findViewById(R.id.start);
+        log = findViewById(R.id.log);
+        profile = findViewById(R.id.profile);
+        setting = findViewById(R.id.setting);
+        logStatus = findViewById(R.id.logStatus);
 
         // 현재 로그아웃 상태
         if(DBManager.GetInstance().mAuth.getCurrentUser()==null) {
             // 텍스트 변경 로그인
-            binding.logStatus.setImageResource(R.drawable.lock);
+            logStatus.setImageResource(R.drawable.lock);
             // 이미지 변경 로그인 이미지
-            binding.log.setText("로그인");
+            log.setText("로그인");
         }
         // 현재 로그인 상태
         else {
-            binding.logStatus.setImageResource(R.drawable.unlock);
-            binding.log.setText("로그아웃");
+            logStatus.setImageResource(R.drawable.unlock);
+            log.setText("로그아웃");
         }
 
-        binding.start.setOnClickListener(new View.OnClickListener() {
+        start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
@@ -50,7 +62,7 @@ public class MainMenuActivity extends AppCompatActivity {
 
             }
         });
-        binding.profile.setOnClickListener(new View.OnClickListener() {
+        profile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
@@ -64,14 +76,14 @@ public class MainMenuActivity extends AppCompatActivity {
             }
         });
 
-        binding.setting.setOnClickListener(new View.OnClickListener() {
+        setting.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainMenuActivity.this, AlarmSettingActivity.class);
                 startActivity(intent);
             }
         });
-        binding.logStatus.setOnClickListener(new View.OnClickListener() {
+        logStatus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // 현재 사용자 로그아웃 상태일경우
