d27608e1c72321152aa7a7d6a1bd267170d2fe85
junlees <junlee820@naver.com>
2024-09-18 22:41:53 +0900

    add login/logout image

--- app/src/main/java/com/example/spda_app/MainActivity2.java
+++ app/src/main/java/com/example/spda_app/MainActivity2.java
@@ -3,9 +3,11 @@ package com.example.spda_app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.spda_app.DAO.DBManager;
 import com.example.spda_app.databinding.MainPageBinding;
 
 public class MainActivity2 extends AppCompatActivity {
@@ -16,12 +18,28 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = MainPageBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        if(DBManager.GetInstance().mAuth.getCurrentUser()==null) {
+            // 텍스트 변경 로그인
+            binding.logStatus.setImageResource(R.drawable.lock);
+            // 이미지 변경 로그인 이미지
+            binding.log.setText("로그인");
+        }
+        // 현재 로그인 상태
+        else {
+            binding.logStatus.setImageResource(R.drawable.unlock);
+            binding.log.setText("로그아웃");
+        }
 
         binding.start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity2.this, OndeviceActivity.class);
-                startActivity(intent);
+                if(DBManager.GetInstance().mAuth.getCurrentUser()==null) {
+                    Toast.makeText(MainActivity2.this, "먼저 로그인을 해주십시오.", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Intent intent = new Intent(MainActivity2.this, OndeviceActivity.class);
+                    startActivity(intent);
+                }
             }
         });
         binding.setting.setOnClickListener(new View.OnClickListener() {
@@ -38,12 +56,32 @@ public class MainActivity2 extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        binding.exit.setOnClickListener(new View.OnClickListener() {
+        binding.logStatus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //finish();
-                System.exit(1);
+                // 현재 사용자 로그아웃 상태일경우
+                if(DBManager.GetInstance().mAuth.getCurrentUser()==null) {
+                    Intent intent = new Intent(MainActivity2.this, LoginActivity.class);
+                    startActivity(intent);
+                }
+                else{
+                    DBManager.GetInstance().mAuth.signOut();
+                    Toast.makeText(MainActivity2.this, "로그아웃 성공", Toast.LENGTH_SHORT).show();
+
+                    // 앱 재시작
+                    Intent intent = new Intent(MainActivity2.this, MainActivity2.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish(); // 현재 액티비티 종료
+                }
             }
         });
+//        binding.exit.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                //finish();
+//                System.exit(1);
+//            }
+//        });
     }
 }
